26222996c5486b54eea0a346c389a5a3021165a0
Max Muth <kontakt@maxi-muth.de>
2023-06-29 21:10:07 +0200

    Update py_macro.py

--- py_macro.py
+++ py_macro.py
@@ -9,22 +9,23 @@ from xvm_main.python.logger import *
 
 
 # todo what is deterministic=False?
-@xvm.export('vehicleDistance')
+@xvm.export('vehicleDistance', deterministic=False)
 def vehicleDistance(name):
     # todo doesn't get updated ever :(
     # name is a player name like "JeffForPresident"
-    print(name)
-    
-    print('player:')
-    player = BigWorld.player()
-    print(player.__dict__)
 
+    print('vehicleDistance called')
+    print('name: ' + name)
+    
+    player = BigWorld.player()
     for vehicle in player.arena.vehicles.itervalues():
         print('vehicle: ' + str(vehicle))
         entity = BigWorld.entity(int(vehicle['avatarSessionID']))
-        print('entity: ' + str(entity.__dict__))
-        print('name: ' + str(entity.publicInfo.name))
+        # todo filter allies
+        # todo only return if entity name == name
         if entity is not None and entity.publicInfo.name == name:
+            print('entity: ' + str(entity.__dict__))
+            print('name: ' + str(entity.publicInfo.name))
             print('player position: ' + str(player.position))
             print('opponent position: ' + str(entity.position))
             print('diff:')
